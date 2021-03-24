e8f5cf3f36a6657fc371d9b3562dee956737ce75
Yuuki Harano <masm+github@masm11.me>
2021-03-24 23:44:30 +0900

    initial project

---
+++ app/src/main/java/me/masm11/contextplayer10/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.masm11.contextplayer10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
