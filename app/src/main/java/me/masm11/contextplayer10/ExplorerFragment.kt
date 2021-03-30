9a3d235a5316937c5d4a61d39d04c5ba4c638480
Yuuki Harano <masm+github@masm11.me>
2021-03-31 00:48:10 +0900

    ExplorerFragment を仮作成

---
+++ app/src/main/java/me/masm11/contextplayer10/ExplorerFragment.kt
@@ -0,0 +1,163 @@
+package me.masm11.contextplayer10
+
+import androidx.fragment.app.Fragment
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.os.Bundle
+import android.os.IBinder
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.widget.SeekBar
+import android.widget.Button
+import android.widget.TextView
+import android.widget.ListView
+import android.widget.Space
+import android.widget.BaseAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import kotlinx.coroutines.*
+
+class ExplorerFragment: Fragment() {
+    private val scope = CoroutineScope(Job() + Dispatchers.Main)
+    private var binder: MainService.Binder? = null
+    private lateinit var listView: ListView
+    
+    // GC に破棄されないよう、変数に持っておく
+    private val listener = object: MainService.OnPlayStatusBroadcastListener {
+	override fun onPlayStatusBroadcastListener(playStatus: Player.PlayStatus) {
+/*
+	    scope.launch {
+		updateInfo(playStatus)
+	    }
+*/
+	}
+    }
+    
+    private val conn = object: ServiceConnection {
+	override fun onServiceConnected(name: ComponentName, binder: IBinder) {
+	    val b = binder as MainService.Binder
+	    this@ExplorerFragment.binder = b
+	    Log.d("connected")
+	    
+	    b.setOnPlayStatusBroadcastedListener(listener)
+	}
+	override fun onServiceDisconnected(name: ComponentName) {
+	    this@ExplorerFragment.binder = null
+	}
+    }
+    
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+	return inflater.inflate(R.layout.explorer_fragment, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+	listView = view.findViewById<ListView>(R.id.list_view)
+    }
+    
+    override fun onStart() {
+	super.onStart()
+	val ctxt = getContext()
+	if (ctxt != null) {
+	    val intent = Intent(ctxt, MainService::class.java)
+	    ctxt.startService(intent)
+	    ctxt.bindService(intent, conn, Context.BIND_AUTO_CREATE)
+
+	    val adapter = ItemAdapter(ctxt, MFile("//primary/nana/impact_exciter"))
+	    listView.setAdapter(adapter)
+	}
+
+//	Item(MFile("//primary/nana/impact_exciter/nana_ie_16.ogg"))
+    }
+    
+    override fun onStop() {
+	super.onStop()
+	val ctxt = getContext()
+	if (ctxt != null)
+	    ctxt.unbindService(conn)
+    }
+    
+
+    inner class ItemAdapter(private val context: Context, private val dir: MFile): BaseAdapter() {
+	private var layoutInflater: LayoutInflater
+	private val itemList = ArrayList<Item>()
+
+	init {
+	    layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+	    
+	    val dirs = ArrayList<Item>()
+	    val files = ArrayList<Item>()
+	    
+	    val list = dir.listFiles()
+	    if (list != null) {
+		list.forEach { m ->
+		    if (m.isDirectory)
+			dirs.add(Item(m, this))
+		    else
+			files.add(Item(m, this))
+		}
+	    }
+	    itemList.add(Item(dir.parentFile, this, ".."))
+	    itemList.addAll(dirs)
+	    itemList.addAll(files)
+	}
+	
+	override fun getCount(): Int {
+	    return itemList.count()
+	}
+	override fun getItem(pos: Int): Item {
+	    return itemList[pos]
+	}
+	override fun getItemId(pos: Int): Long {
+	    return getItem(pos).id
+	}
+	override fun getView(pos: Int, convertView: View?, parent: ViewGroup): View {
+	    val view = if (convertView == null) layoutInflater.inflate(R.layout.explorer_item, parent, false) else convertView
+	    
+	    val item = getItem(pos)
+	    view.findViewById<TextView>(R.id.name).setText(item.name)
+	    view.findViewById<TextView>(R.id.title).setText(item.metadata.title)
+	    view.findViewById<TextView>(R.id.artist).setText(item.metadata.artist)
+	    
+            return view
+	}
+    }
+    
+    inner class Item(val path: MFile, private val adapter: ItemAdapter, private val name0: String? = null) {
+	var metadata: Metadata
+	private set
+	
+	var name: String
+	private set
+	
+	init {
+	    metadata = Metadata(path.file.toString())
+	    scope.launch {
+		metadata.extract()
+		withContext(Dispatchers.Main) {
+		    Log.d("${metadata.title} / ${metadata.artist}")
+		    adapter.notifyDataSetChanged()
+		}
+	    }
+
+	    if (name0 == null) {
+		val str = path.toString()
+		val slashPos = str.lastIndexOf('/')
+		name = str.substring(slashPos + 1)
+	    } else {
+		name = name0
+	    }
+	}
+	
+	val id: Long
+	get() {
+	    return path.hashCode().toLong()
+	}
+	
+	val isDirectory: Boolean
+	get() {
+	    return path.isDirectory
+	}
+    }
+}
